b3f65d6d532d7fc5a10b513907e8a0b5db59af9d
Ilya Savelev <ilyasavelyev98@mail.ru>
2025-10-14 22:17:09 +0300

    upd

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import datetime
 
 
 if __name__ == "__main__":
+    date = datetime.date.today()
+
     all_stock = mbtk.AllStockInfo(path=r'support_files/rates_all.csv')
     splitter_vtb = mbtk.VtbSplitter()
     splitter_vtb.split(r'.reports/vtb_20250917_20251012.xlsx')
@@ -30,13 +32,13 @@ if __name__ == "__main__":
         r'support_files/index_fund.xlsx', 
         all_stock_info=all_stock
         )
-    ds.get_table().to_excel(r'.output/dt.xlsx', index=False)
+    ds.get_table().to_excel(f'.output/dt_{date}.xlsx', index=False)
 
     br = mbtk.BalanceReport(
         report_registry=report_registry,
         distribution_table=ds
         )
-    br.get_balance_report().to_excel(r'.output/br.xlsx', index=False)
+    br.get_balance_report().to_excel(f'.output/br_{date}.xlsx', index=False)
 
     ta = mbtk.TargetAllocator(
         distribution_table=ds,
@@ -48,7 +50,7 @@ if __name__ == "__main__":
 
     df = ta.get_distrib_of_money_df()
 
-    df.to_excel(r'.output/TargetAllocator.xlsx', index=False)
+    df.to_excel(f'.output/TargetAllocator_{date}.xlsx', index=False)
     print(df)
     print(ta.get_money_count())
     print(ta.work_log)
@@ -58,7 +60,7 @@ if __name__ == "__main__":
         template_path=r'moex_broker_toolkit/templates/md_template.md')
     rg = mbtk.ReportGenerator(rs)
     rg.generate_report()
-    date = datetime.date.today()
+    
     rg.save_report(f'/Users/ilya/Documents/svlv_storage/SVLV_notebook/00_Notes/broker_report_{date}.md')
 
 
